36dc49369e888559b241e17dddd7b7ade0eec5a6
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-06-16 16:31:41 +0530

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tf-bucket-prod1"
-    key    = "uc9-new/terraform.tfstate"
+    key    = "uc9-latest/terraform.tfstate"
     region = "us-east-1"
     #use_lockfile = true
     encrypt = true
